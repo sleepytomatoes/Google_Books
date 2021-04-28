94b3c6c3a55c8bd375d59000826097b8ff5e79b1
Shauna Dunn <shaunadunn1@gmail.com>
2021-04-28 19:54:37 -0400

    deploying

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/GoogleBook',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
 // Define API routes here
 
 // Send every other request to the React app
